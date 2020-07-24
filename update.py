95a82b2444fe16b9a1720b7008cd7c0176eee4a6
Sebastian Müller <58611001+peterQpan@users.noreply.github.com>
2020-07-24 02:01:51 +0200

    Add files via upload
    Abstract Class to enable Updates, later addition of class attributes of already saved/pickled save files.
    Need to be able to do recursion, since tasks can have tasks as subtasks

---
+++ update.py
@@ -0,0 +1,53 @@
+__author__ = "Sebastian Müller"
+__copyright__ = "Just Me"
+__email__ = "sebmueller.bt@gmail.com"
+
+import pickle
+
+class Upgradeable:
+    """
+    Abstract Class to make introduction of new Attributes possible
+    """
+    class_version = 1.0
+    """class attribute to make versioning global
+    starts at 1 zero is without Upgradeable """
+
+    # todo this class is upgradable but it cant handle recursive class attributes,
+    #  make a new recursive upgradeable class or try to add a recursive upgrade method
+
+    def __init__(self):
+        self.version = self.__class__.class_version
+
+    def save(self, file_name="test_update.bin"):
+        with open(file_name, "wb") as fh:
+            pickle.dump(self, fh)
+
+    @staticmethod
+    def load(file_name="test_update.bin"):
+        with open(file_name, "rb") as fh:
+            out = pickle.load(fh)
+        if not hasattr(out, 'version'):
+            out.version = 0.0
+        if out.version != out.class_version:
+            out.upgrade()
+        return out
+
+    def upgrade(self):
+        print(f"upgrade from version {self.version}")
+        if self.version < 1.0:
+            self.version = 1.0
+        #     self.in_version_1_0_added_atribute = "what ever"
+        #     self.in_version_1_0_added_atribute2 = "what ever"
+        #     print('upgrade to version 1.0')
+        # if self.version < 2.0:
+        #     self.version = 2.0
+        #     self.in_version_2_0_added_atribute = "what ever"
+        #     self.in_version_2_0_added_atribute2 = "what ever"
+        #     print('upgrade to version 2.0')
+        # if self.version < 3.0:
+        #     self.version = 3.0
+        #     self.in_version_3_0_added_atribute = "what ever"
+        #     self.in_version_3_0_added_atribute2 = "what ever"
+        #     print('upgrade to version 3.0')
+
+
