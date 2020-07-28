a6e473a58de2d18bb8b8c5ffb5aa73ec9354c72c
ich <blaupause1713@gmail.com>
2020-07-28 12:54:14 +0200

    abstract persistence class

--- upgrade.py
+++ upgrade.py
@@ -4,38 +4,27 @@ __email__ = "sebmueller.bt@gmail.com"
 
 import pickle
 
-
-class Upgrader:
-    """
-    Abstract Class to make introduction of new Attributes possible
-    """
-    class_version = 1.0
-    """class attribute to make versioning global
-    starts at 1 zero is without Upgradeable """
-
-    # todo this class is upgradable but it cant handle recursive class attributes,
-    #  make a new recursive upgradeable class or try to add a recursive upgrade method
+class Persistencer:
+    """Abstract Class which handles persistence, including introducing new attributes
+    for further development:
+    Abstract upgrade class for Task, for unpickeling old save data with
+    inadequate/insufficient attributes (able to do upgrades recursively)"""
+    sub_tasks: list  # inherited then by task.Task()
+    class_version = 1.0 # class attribute to make versioning global starts at 1 zero is without Upgradeable"""
 
     def __init__(self):
         self.version = self.__class__.class_version
 
-    # not needed Taskmanager takes care of saving
-    # ok with single responsibility?!?
-    # def save(self, file_name="test_update.bin"):
-    #     with open(file_name, "wb") as fh:
-    #         pickle.dump(self, fh)
+    def save(self, file_name="test_update.bin"):
+        with open(file_name, "wb") as fh:
+            pickle.dump(self, fh)
 
     @staticmethod
-    def load(file_name="test_update.bin"):
-        with open(file_name, "rb") as fh:
-            out = pickle.load(fh)
-        if not hasattr(out, 'version'):
-            out.version = 0.0
-        if out.version != out.class_version:
-            out.upgrade()
-        return out
+    def _recursivUpgrade(out):
+        if out.sub_tasks:
+            [sub_task._upgrade() for sub_task in out.sub_tasks]
 
-    def upgrade(self):
+    def _upgrade(self):
         print(f"upgrade from version {self.version}")
         if self.version < 1.0:
             self.version = 1.0
@@ -53,22 +42,21 @@ class Upgrader:
         #     self.in_version_3_0_added_atribute2 = "what ever"
         #     print('upgrade to version 3.0')
 
-
-class TaskUpgrader(Upgrader):
-    """Abstract upgrade class for Task (able to do upgrades recursively)"""
-    sub_tasks: list  # inherited then by task.Task()
-
     @staticmethod
-    def versionSaveUnpickeling(pickle_thing):
-        out = pickle.load(pickle_thing)
-        if not hasattr(out, 'version'):
-            out.version = 0.0
-        out.upgrade()
+    def _properLoad(file_name):
+        with open(file_name, "rb") as fh:
+            out = pickle.load(fh)
+        out.version = out.__dict__.get("version", 0.0)
         return out
 
-    def upgrade(self):
-        """recursiv upgrade version"""
-        print(f"upgrade from version {self.version}")
-        if self.version < 1.0:
-            self.version = 1.0
-        #     self.in_version_1_0_added_atribute = "what ever"
+    @staticmethod
+    def load(file_name="test_update.bin"):
+        out = Persistencer._properLoad(file_name)
+        if out.version != out.class_version:
+            out._upgrade()
+            out._recursivUpgrade(out)
+        return out
+
+
+
+
